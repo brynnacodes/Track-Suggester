5cfb0a7139541cfdbac88df60a10a7aa6a7c1b60
Brynna McCarter <brynna.mccarter@gmail.com>
2017-01-13 10:37:27 -0800

    fix jquery logic to work for boolean form

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,32 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var nameInput = $("input#name").val();
 
+    var question1 = $("input#question1").val();
+    var question2 = $("input#question2").val();
+    var question3 = $("input#question3").val();
 
+
+    if (question1 === "true") {
+      $("#results-Java").show();
+    } else if (question2 === "true") {
+      $("#results-Ruby").show();
+    } else if (question3 === "true") {
+      $("#results-C").show();
+    }
+
+    //
+    // if (question2==="true") {
+    //   $("results-Ruby").show();
+    // } else {
+    //   $("results-Ruby").hide();
+    // }
+    //
+    //
+    // if (question3==="true") {
+    //   $("results-C").show();
+    // } else {
+    //   $("results-C").hide();
+    // }
 
     $("#results-name").text(nameInput);
     event.preventDefault();
