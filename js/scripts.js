60579434167bb1070a7808c478f51a699ddfe6a3
Brynna McCarter <brynna.mccarter@gmail.com>
2017-01-13 11:03:12 -0800

    add two additional true/false questions to html and additonal branches in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-//business logic:
-
-
-//user interface logic:
 
 $(document).ready(function() {
   $("form").submit(function(event) {
@@ -10,29 +6,25 @@ $(document).ready(function() {
     var question1 = $("input#question1").val();
     var question2 = $("input#question2").val();
     var question3 = $("input#question3").val();
+    var question4 = $("input#question4").val();
+    var question5 = $("input#question5").val();
 
 
     if (question1 === "true") {
       $("#results-Java").show();
+
     } else if (question2 === "true") {
       $("#results-Ruby").show();
+
     } else if (question3 === "true") {
       $("#results-C").show();
-    }
 
-    //
-    // if (question2==="true") {
-    //   $("results-Ruby").show();
-    // } else {
-    //   $("results-Ruby").hide();
-    // }
-    //
-    //
-    // if (question3==="true") {
-    //   $("results-C").show();
-    // } else {
-    //   $("results-C").hide();
-    // }
+    } else if (question4 === "true") {
+      $("#results-PHP").show();
+
+    } else if (question5 === "true") {
+      $("#results-Design").show();
+    }
 
     $("#results-name").text(nameInput);
     event.preventDefault();
