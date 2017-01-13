9721b4f0168fb67945edd05f7e12bd15eea766f6
Brynna McCarter <brynna.mccarter@gmail.com>
2017-01-13 12:27:25 -0800

    add bootstrap/custom css styles to quiz, add jquery fade effects

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,26 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     var nameInput = $("input#name").val();
-
     var question1 = $("input#question1").val();
     var question2 = $("input#question2").val();
     var question3 = $("input#question3").val();
     var question4 = $("input#question4").val();
     var question5 = $("input#question5").val();
 
-
     if (question1 === "true") {
-      $("#results-Java").show();
+      $("#results-Java").fadeToggle();
 
     } else if (question2 === "true") {
-      $("#results-Ruby").show();
+      $("#results-Ruby").fadeToggle();
 
     } else if (question3 === "true") {
-      $("#results-C").show();
+      $("#results-C").fadeToggle();
 
     } else if (question4 === "true") {
-      $("#results-PHP").show();
+      $("#results-PHP").fadeToggle();
 
     } else if (question5 === "true") {
-      $("#results-Design").show();
+      $("#results-Design").fadeToggle();
     }
 
     $("#results-name").text(nameInput);
